1be266172293dacdd35062fc9fc4d992a9f953c2
Patrick Hughes <patrick.hughes@coinbase.com>
2025-05-14 14:03:44 -0700

    update game contract

--- minikit/three-card-monte/lib/constants.ts
+++ minikit/three-card-monte/lib/constants.ts
@@ -1,9 +1,12 @@
 export const GAME_CONTRACT_ADDRESS_SEPOLIA =
-  "0x352D55dd643ec049191d133a146bE3086e0e1B85";
+  "0x7eFc99DF7e8a2b01E2E3d905c37e45F8d3FB227F";
 
 export const PAYMASTER_URL = process.env.NEXT_PUBLIC_PAYMASTER_URL as string;
 export const PAYMASTER_URL_SEPOLIA =
   process.env.NEXT_PUBLIC_PAYMASTER_URL_SEPOLIA;
+
+export const CDP_SIGNER_ADDRESS = process.env
+  .CDP_SIGNER_ADDRESS as `0x${string}`;
 
 // Fee required to start a game (in ETH)
 export const PLAY_FEE_ETH = "0.00001"; // 0.0001 ETH
