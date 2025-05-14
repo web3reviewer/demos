bc7978863bcdfdac57e240953cca456c500e247f
Patrick Hughes <patrick.hughes@coinbase.com>
2025-05-13 22:01:13 -0700

    add abi and constants file

---
+++ minikit/three-card-monte/lib/constants.ts
@@ -0,0 +1,11 @@
+export const GAME_CONTRACT_ADDRESS_SEPOLIA =
+  "0x352D55dd643ec049191d133a146bE3086e0e1B85";
+
+export const PAYMASTER_URL = process.env.NEXT_PUBLIC_PAYMASTER_URL as string;
+export const PAYMASTER_URL_SEPOLIA =
+  process.env.NEXT_PUBLIC_PAYMASTER_URL_SEPOLIA;
+
+// Fee required to start a game (in ETH)
+export const PLAY_FEE_ETH = "0.00001"; // 0.0001 ETH
+export const PLAY_FEE_USDC = "0.1"; // 10 cents
+export const REWARD_AMOUNT = "0.0001"; // 0.0001 ETH
